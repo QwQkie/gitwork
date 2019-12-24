46d5d3d45bb235015d1b8fcef0cf582d1185a5fe
xiexin <2675760452@qq.com>
2019-12-24 19:28:38 +0800

    书籍

---
+++ src/main/java/com/briup/gitwork/web/controller/XiexinController.java
@@ -0,0 +1,33 @@
+package com.briup.gitwork.web.controller;
+
+import com.briup.gitwork.util.Message;
+import com.briup.gitwork.util.MessageUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(description = "图书管理系统")
+public class XiexinController {
+    @PostMapping("/add")
+    @ApiOperation(value ="添加书籍" )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Id",value = "书号",paramType = "query",dataType = "int"),
+            @ApiImplicitParam(name = "name",value = "书名",paramType = "query",dataType = "String",required = true)
+    })
+    public Message add(int Id, String name){
+        return MessageUtil.success("添加书籍成功");
+
+    }
+
+    @PostMapping("/delet")
+    @ApiOperation(value ="删除书籍" )
+    public Message delet(int Id, String name ){
+        return MessageUtil.success("删除书籍成功");
+    }
+
+
+}
