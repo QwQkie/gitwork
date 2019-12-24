0befd9dbd3e7dea97bbc44829700417423997946
xiaoqianlong <1582529844@qq.com>
2019-12-24 17:02:49 +0800

    初始化项目

---
+++ src/main/java/com/briup/gitwork/GitworkApplication.java
@@ -0,0 +1,13 @@
+package com.briup.gitwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitworkApplication.class, args);
+    }
+
+}
